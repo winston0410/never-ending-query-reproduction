03c0b27c4351f0c1ed6a4c13aab76a04b7e5be3e
John Doe <hugosum.dev@protonmail.com>
2024-02-18 08:59:58 +0000

    init

---
+++ index.ts
@@ -0,0 +1,39 @@
+import { Surreal } from 'surrealdb.node'
+
+(async () => {
+    // Start Surreal with the following command
+    // surreal start --user root --pass root file:database.db --auth
+    const db = new Surreal();
+
+    const connector = {
+        id: 'default',
+        provider: 'surrealDB',
+        namespace: 'test',
+        dbName: 'test',
+        url: 'ws://127.0.0.1:8000',
+        username: 'root',
+        password: 'root'
+    }
+
+    await db.connect(connector.url)
+    await db.signin({
+        namespace: connector.namespace,
+        database: connector.dbName,
+        username: connector.username,
+        password: connector.password,
+    })
+
+    try {
+        console.log('before query')
+
+        await db.query(`
+        DEFINE TABLE foobar SCHEMALESS;
+        DEFINE FIELD name ON TABLE foobar TYPE string;
+        DEFINE FIELD email ON TABLE foobar TYPE string;
+        `)
+
+        console.log('after query')
+    } catch (e) {
+        console.error(e)
+    }
+})()
